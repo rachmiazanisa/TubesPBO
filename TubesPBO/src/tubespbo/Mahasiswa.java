916f97f2a1270e19ad93ed75f73b2670ab1f2ea5
Rachmi <Rachmi@10.5.250.4>
2016-03-18 11:57:30 +0700

    penambahan class Tugas Akhir part 2

---
+++ TubesPBO/src/tubespbo/Mahasiswa.java
@@ -0,0 +1,29 @@
+package tubespbo;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Rachmi
+ */
+public class Mahasiswa {
+    private TugasAkhir tugasAkhir;
+    private String statusMahasiswa;
+    private String nim;
+   
+    public void setNim(String nim){
+        this.nim=nim;
+    }
+    
+    public String getNim(){
+        return nim;
+    }
+    
+    public void createTA(String judul){
+        TugasAkhir tugasAkhir=new TugasAKhir(judul);
+    }
+}
